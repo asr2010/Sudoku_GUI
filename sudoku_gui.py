eab6e3d5183e4f139ef9638d43fe0adeb475d762
Arpit Raj <arpitshekharraj@gmail.com>
2022-02-18 15:09:26 -0500

    Bactrack Complete

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -11,32 +11,51 @@ response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
 grid = response.json()['board']
 grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
 
-def insert (win, position):
-    i,j = position[1], position[0]
-    myfont = pygame.font.SysFont('Comic Sans MS', 35)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                #1. tire to edit original file
-                if(grid_original[i-1][j-1] != 0):
-                    return
-                #2. edit
-                if(event.key == 48): #checking with 0
-                    grid[i-1][j-1]=event.key -48
-                    pygame.draw.rect(win, background_color, (position[0] * 50 + buffer, position[1] * 50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
-                    pygame.display.update()
-                if(0 < event.key - 48 < 10 ): #checking for valid input
-                    pygame.draw.rect(win, background_color, (position[0] * 50 + buffer, position[1] * 50 + buffer, 50 - 2*buffer, 50 - 2*buffer))
-                    value = myfont.render(str(event.key-48), True, (0,0,0))
-                    win.blit(value, (position[0]*50 + 15, position[1]*50))
-                    grid[i-1][j-1] = event.key - 48
-                    pygame.display.update()
-                    return
-                return
-                #3. adding the digits
+def isEmpty(num):
+    if(num == 0):
+        return True
+    return False
+def isValid(position, num):
+    #Row, Col and Box Checks
+    for i in range(0, len(grid[0])):
+        if(grid[position[0]][i] == num):
+            return False
+    for i in range(0,len(grid[0])):
+        if(grid[i][position[1]] == num):
+            return False
+    x = position[0]//3*3
+    y = position[1]//3*3
 
+    for i in range(0,3):
+        for j in range(0,3):
+            if(grid[x+i][y+j] == num):
+                return False
+    return True
+
+solved =0
+def sudoku_solver(win):
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    for i in range(0, len(grid[0])):
+        for j in range(0, len(grid[0])):
+            if(isEmpty(grid[i][j])):
+                for k in range(1,10):
+                    if isValid((i,j),k):
+                        grid[i][j] = k
+                        value = myfont.render(str(k), True, (0,0,0))
+                        win.blit(value, (((j+1)*50 +15,(i+1)*50)))
+                        pygame.display.update()
+                        #pygame.time.delay(1)
+
+                        sudoku_solver(win)
+                        global solved
+                        if(solved == 1):
+                            return
+
+                        grid[i][j] = 0
+                        pygame.draw.rect(win, background_color, ((j+1)*50 + buffer,(i+1)*50 + buffer, 50 -2*buffer, 50 - 2*buffer))
+                        pygame.display.update()
+                return
+    solved =1
 
 def main():
     pygame.init()
@@ -60,11 +79,9 @@ def main():
                 win.blit(value, ((j+1)*50 +15, (i+1)*50))
     pygame.display.update()
 
+    sudoku_solver(win)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                pos = pygame.mouse.get_pos()
-                insert(win, (pos[0]//50, pos[1]//50))
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
